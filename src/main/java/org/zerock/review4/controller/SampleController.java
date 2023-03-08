5ac13ccabe0226a7d659880369fee30ebbfb3d7a
myo22 <dlalsgud12@naver.com>
2023-03-08 14:11:10 +0900

    Thymeleaf

--- src/main/java/org/zerock/review4/controller/SampleController.java
+++ src/main/java/org/zerock/review4/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController { //스프링 부트를 이용하여 웹을 개발하는 일은 컨트롤러나 화면을 개발하는 것과 유사하지만 web.xml이나 servlet-context.xml가 같은 웹 관련 설정 파일들이 없기 때문에
@@ -14,5 +17,13 @@ public class SampleController { //스프링 부트를 이용하여 웹을 개발
         log.info("hello.........................");
 
         model.addAttribute("msg", "HELLO WORLD");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD"); //Array를 List로 변환하는 것이다.
+
+        model.addAttribute("list", list);
     }
 }
