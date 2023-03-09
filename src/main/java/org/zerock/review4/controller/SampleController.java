c7f360577bd4b92b6fb90448a0a42b3a88d444bf
myo22 <dlalsgud12@naver.com>
2023-03-09 23:58:51 +0900

    Thymeleaf 인라인 기능

--- src/main/java/org/zerock/review4/controller/SampleController.java
+++ src/main/java/org/zerock/review4/controller/SampleController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -25,5 +29,47 @@ public class SampleController { //스프링 부트를 이용하여 웹을 개발
         List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD"); //Array를 List로 변환하는 것이다.
 
         model.addAttribute("list", list);
+    }
+
+    class SampleDTO{
+        private String p1, p2, p3;
+
+        public String getP1(){
+            return p1;
+        }
+
+        public String getP2(){
+            return p2;
+        }
+
+        public String getP3(){
+            return p3;
+        }
+    }
+
+    //Thymeleaf의 고유한 기능들(인라인 기능)
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model){
+
+        log.info("ex/ex2.....................");
+
+        List<String> strList = IntStream.range(1, 10) // 1에서 10까지 반복.
+                .mapToObj(i -> "Data" + i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+
+        model.addAttribute("dto", sampleDTO);
     }
 }
